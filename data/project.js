d00923d3f3caf423d047ef82222dbd0ffafb111a
harsh0292 <harshgarg02092002@gmail.com>
2023-09-01 19:44:06 +0530

    project added

--- data/project.js
+++ data/project.js
@@ -2,14 +2,14 @@ const projectsData = [{
         title: 'Song Surge',
         description: 'Crafted an impressive Music Streaming Website through Front-End expertise, delivering a user friendly interface and with a focus on seamless audio streaming and an intutive user-interface.',
         imageUrl: '../images/plinth.png',
-        link: 'https://www.plinth.co.in/',
+        link: 'https://songsurge-harsh.netlify.app/',
         buttontitle: "Website",
     },
     {
         title: 'Personal Portfolio',
         description: 'This is my personal portfolio. I have developed as well as designed this on my own',
         imageUrl: '../images/pfl.png',
-        link: 'https://www.aryanbansal02.netlify.app/',
+        link: 'https://www.harsh020902.netlify.app/',
         buttontitle: "Website",
     },
     {
@@ -17,7 +17,7 @@ const projectsData = [{
         description: 'Crafted a captivating E-Commerce Website solely through Front-End Devlopment showcasing an intutive User Interface, seamless navigation, and a visually engaging shoping experience.',
         imageUrl: '../images/robot.jpg',
         link: 'https://github.com/Aryan0169/Gesture-Control-Bot.git',
-        buttontitle: "GITHUB",
+        buttontitle: "Website",
     },
     {
         title: 'Leetcode Profile',
