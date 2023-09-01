f872a5f85bc338349e9d40a70aff31116fbefc4c
harsh0292 <harshgarg02092002@gmail.com>
2023-09-01 19:14:59 +0530

    projects remain

--- data/project.js
+++ data/project.js
@@ -1,7 +1,6 @@
-const projectsData = [
-    {
-        title: 'PLINTH 2023',
-        description: 'Worked as a UI/UX Developer in making the website for the Annual techfest of LNMIIT, Jaipur.',
+const projectsData = [{
+        title: 'Song Surge',
+        description: 'Crafted an impressive Music Streaming Website through Front-End expertise, delivering a user friendly interface and with a focus on seamless audio streaming and an intutive user-interface.',
         imageUrl: '../images/plinth.png',
         link: 'https://www.plinth.co.in/',
         buttontitle: "Website",
@@ -14,32 +13,18 @@ const projectsData = [
         buttontitle: "Website",
     },
     {
-        title: 'Gesture Controlled BOT',
-        description: 'As a Project for Design Lab in III Sem of college, I created a bot that moves according to the motion of the Hand',
+        title: 'Swift Mart',
+        description: 'Crafted a captivating E-Commerce Website solely through Front-End Devlopment showcasing an intutive User Interface, seamless navigation, and a visually engaging shoping experience.',
         imageUrl: '../images/robot.jpg',
         link: 'https://github.com/Aryan0169/Gesture-Control-Bot.git',
         buttontitle: "GITHUB",
     },
     {
-        title: 'Design Projects',
-        description: 'This is my Behance Profile which contain all my design projects',
+        title: 'Leetcode Profile',
+        description: 'This is my Leetcode Profile which shows my coding skills.',
         imageUrl: '../images/behance.png',
         link: 'https://www.behance.net/aryanbansal6',
-        buttontitle: "Behance",
+        buttontitle: "Leetcode",
     },
-    {
-        title: 'UAV assisted multiuser DF relayed system',
-        description: 'As a LUSIP summer intern I made use of NOMA technology combined with relay mechanism to improve 6G+ communication',
-        imageUrl: '../images/wireless.jpg',
-        link: 'https://drive.google.com/file/d/14mNKAJsk931b5jZ85hwq7dHyWdEPsR_X/view?usp=drive_link',
-        buttontitle: "Certificate",
-    },
-    {
-        title: 'GDSC Website',
-        description: 'As the creative domain lead at GDSC LNMIIT, I designed the website for our club',
-        imageUrl: '../images/gdsc.png',
-        link: 'https://www.figma.com/proto/PFQnOfa19onhSRTomS1Rz7/Website-GDSC?type=design&node-id=612-23658&t=AVq4nIfDq2iAXkXP-0&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=640%3A2515',
-        buttontitle: "Figma",
-    }
     // Add more project objects as needed
 ];
